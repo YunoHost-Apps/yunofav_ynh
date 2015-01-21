dea0b867cd6af9e41d144b7eef76938b7603431f
Chtixof <chtixof@gmx.fr>
2015-01-21 22:10:23 +0100

    Cache cleaning in php response

--- sources/fav.php
+++ sources/fav.php
@@ -2,6 +2,9 @@
 $text=file_get_contents('php://input');
 if ($text!="") {
   file_put_contents('fav.json', $text);
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+  header("Content-Type: application/json; charset=utf-8");
   echo($text);
 }
 ?>
